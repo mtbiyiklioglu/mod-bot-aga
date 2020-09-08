0a38aae9a5776669e1b273b0278a6b2c32ca37e7
Mete Bıyıklıoğlu <mete.biyiklioglu2008@gmail.com>
2020-09-08 16:24:03 +0300

    change avatar displayAv

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ bot.on('message' , msg=>{
         let Embed = new Discord.MessageEmbed()
         .setTitle('Avatarın!')
         .setColor('RANDOM')
-        .setDescription(msg.author.displayAvatarUrl())
+        .setDescription(msg.author.displayAvatarUrl)
         msg.channel.send(Embed)
     }
     if(msg.content === prefix + 'botinfo'){
